ccfe101162068f5ea9559dbf297c2aa412cd1f3a
GirishEgo <Jai.girishego@gmail.com>
2025-03-04 16:13:53 +0530

    carousel text added

--- src/Components/Hero/Carousel.jsx
+++ src/Components/Hero/Carousel.jsx
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion"; // Import Framer Motion
 import "./carousel.css"; // Import the CSS file
-// import sliderImages4 from "../../assets/Images/slider banner/imgs (4).png";
 
 const Carousel = () => {
   const images = [
     "https://t3.ftcdn.net/jpg/09/35/11/38/360_F_935113876_ZuJdvJbR8Vgn1lgNgvYk5kMCstzl4IZD.jpg",
-    "https://vijaytransformers.com/images/main-slider/1.jpg",
     "https://www.electricalpanelmanufacturers.com/images/banner-two.jpg",
-    // sliderImages4,
+    "https://vijaytransformers.com/images/main-slider/1.jpg",
   ];
+
+  const texts = [
+    "EXTENSIVE RANGE OF ANTI-CONDENSATION PANEL HEATERS & THERMOSTATS",
+    "SPACE HEATING & CONTROL SOLUTIONS FOR PANEL BOARDS & SWITCHGEARS",
+    "CLIMATE CONTROL SOLUTIONS FOR ENCLOSURES, MARSHALLING BOXES & CABINETS",
+  ];
+
+  const positions = ["top-left", "top-right", "bottom-center"];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -20,16 +27,6 @@ const Carousel = () => {
     return () => clearInterval(interval);
   }, [images.length]);
 
-  // const handleNext = () => {
-  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  // };
-
-  // const handlePrev = () => {
-  //   setCurrentIndex(
-  //     (prevIndex) => (prevIndex - 1 + images.length) % images.length
-  //   );
-  // };
-
   return (
     <div className="carousel">
       <div className="image-container">
@@ -38,16 +35,10 @@ const Carousel = () => {
           alt={`Slide ${currentIndex + 1}`}
           className="carousel-image"
         />
+        <div className={`text-overlay ${positions[currentIndex]}`}>
+          {texts[currentIndex]}
+        </div>
       </div>
-      {/* Buttons */}
-      {/* <button onClick={handlePrev} className="carousel-button prev-button">
-        Previous
-      </button>
-      <button onClick={handleNext} className="carousel-button next-button">
-        Next
-      </button> */}
-
-      {/* <AnimatedElements /> */}
     </div>
   );
 };
